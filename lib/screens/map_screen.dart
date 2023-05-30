1393437789d057881e50ddd8d08afb29614a82ea
Lans12345 <olanalans12345@gmail.com>
2023-05-30 12:23:20 +0800

    added map view

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:location_tracker/widgets/text_widget.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 import '../widgets/drawer_widget.dart';
+import 'package:latlong2/latlong.dart';
 
 class MainMap extends StatefulWidget {
   const MainMap({super.key});
@@ -14,31 +15,31 @@ class _MainMapState extends State<MainMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(
-        child: DrawerWidget(),
-      ),
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Sample Google Map',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+        drawer: const Drawer(
+          child: DrawerWidget(),
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Sample Google Map',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(8.477217, 124.645920),
+            zoom: 16.0,
+          ),
           children: [
-            TextRegular(
-                text: '\n\n\n\n\n\n\n\n\n\nDisplay Map',
-                fontSize: 25,
-                color: Colors.black),
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+            ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }

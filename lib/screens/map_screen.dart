acedbce00efaf89d0b12d8a28c2829248c7a2740
Lans12345 <olanalans12345@gmail.com>
2023-06-01 07:58:48 +0800

    added map page

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../widgets/drawer_widget.dart';
-import 'package:latlong2/latlong.dart';
 
 class MainMap extends StatefulWidget {
   const MainMap({super.key});
@@ -13,33 +16,102 @@ class MainMap extends StatefulWidget {
 
 class _MainMapState extends State<MainMap> {
   @override
+  void initState() {
+    super.initState();
+    determinePosition();
+    getLocation();
+  }
+
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  late double lat = 0;
+  late double long = 0;
+  bool hasLoaded = false;
+
+  getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      lat = position.latitude;
+      long = position.longitude;
+      hasLoaded = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    CameraPosition kGooglePlex = CameraPosition(
+      target: LatLng(lat, long),
+      zoom: 14.4746,
+    );
     return Scaffold(
-        drawer: const Drawer(
-          child: DrawerWidget(),
-        ),
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'Home Screen',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+      drawer: const Drawer(
+        child: DrawerWidget(),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Home Screen',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(8.477217, 124.645920),
-            zoom: 16.0,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.app',
+      ),
+      body: hasLoaded
+          ? GoogleMap(
+              buildingsEnabled: true,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              mapType: MapType.normal,
+              initialCameraPosition: kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
             ),
-          ],
-        ));
+    );
+  }
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
